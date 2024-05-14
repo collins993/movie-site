dc0bcf64b156e0137578b20fc17d043b7c141f3f
Collins Nze <74765532+collins993@users.noreply.github.com>
2024-05-14 12:53:35 +0100

    Adding about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+      <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Consectetur quidem dolorem eos debitis sint iusto architecto et eaque facilis,
+         laborum quisquam adipisci provident itaque autem molestias tempora ex quasi doloremque.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Consectetur quidem dolorem eos debitis sint iusto architecto et eaque facilis,
+         laborum quisquam adipisci provident itaque autem molestias tempora ex quasi doloremque.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Consectetur quidem dolorem eos debitis sint iusto architecto et eaque facilis,
+         laborum quisquam adipisci provident itaque autem molestias tempora ex quasi doloremque.</p>
+
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Consectetur quidem dolorem eos debitis sint iusto architecto et eaque facilis,
+         laborum quisquam adipisci provident itaque autem molestias tempora ex quasi doloremque.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Consectetur quidem dolorem eos debitis sint iusto architecto et eaque facilis,
+         laborum quisquam adipisci provident itaque autem molestias tempora ex quasi doloremque.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Consectetur quidem dolorem eos debitis sint iusto architecto et eaque facilis,
+         laborum quisquam adipisci provident itaque autem molestias tempora ex quasi doloremque.</p>
+
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Consectetur quidem dolorem eos debitis sint iusto architecto et eaque facilis,
+         laborum quisquam adipisci provident itaque autem molestias tempora ex quasi doloremque.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Consectetur quidem dolorem eos debitis sint iusto architecto et eaque facilis,
+         laborum quisquam adipisci provident itaque autem molestias tempora ex quasi doloremque.Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Consectetur quidem dolorem eos debitis sint iusto architecto et eaque facilis,
+         laborum quisquam adipisci provident itaque autem molestias tempora ex quasi doloremque.</p>
+    </div>
+  )
+}
