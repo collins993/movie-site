ea7f29e56ace808bf90042f50bbfed4872066b89
Collins Nze <74765532+collins993@users.noreply.github.com>
2024-05-18 11:42:48 +0100

    add loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,21 @@ import Results from "./components/Results";
 
 const API_KEY = process.env.API_KEY;
 
-export default async function Home({searchParams}) {
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  
+    const res = await fetch(
+      `https://api.themoviedb.org/3${
+        genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
+      }?api_key=${API_KEY}&language=en-US&page=1`,
+      { next: { revalidate: 10000 } }
+    );
+ 
 
-  const genre = searchParams.genre || 'fetchTrending';
-  const res = await fetch(`https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`}?api_key=${API_KEY}&language=en-US&page=1`);
   const data = await res.json();
 
-  if(!res.ok){
-    throw new Error('Faile to fetch data');
+  if (!res.ok) {
+    throw new Error("Faile to fetch data");
   }
 
   const results = data.results;
@@ -19,5 +26,5 @@ export default async function Home({searchParams}) {
     <div>
       <Results results={results} />
     </div>
-  )
+  );
 }
